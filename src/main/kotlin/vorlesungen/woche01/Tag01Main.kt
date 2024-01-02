3dcbd4f80c984e7bf22a31d4dc660face55736ad
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-01-02 09:57:11 +0000

    erste printlns und prints geschrieben, compielfehler gezeigt

--- src/main/kotlin/vorlesungen/woche01/Tag01Main.kt
+++ src/main/kotlin/vorlesungen/woche01/Tag01Main.kt
@@ -4,8 +4,36 @@ package vorlesungen.woche01
     main Funktion: der Ort, an dem unser Code ausgeführt wird
     jeglicher Code muss hier rein (zumindest, bis wir weitere Funktionen kennenlernen)
  */
+
+// geschweifte Klammer { : option 8
 fun main() {
 
-    println("hi")
+    // einzeiliger Kommentar (forward slash /: shift 7)
+    println("Hi")
+
+    /* (stern * : shift +)
+    mehrzeiliger
+    kommentar
+     */
+
+    println("Wie geht's")
+
+    // unsere 1. Zeile Code
+    // println: druckt Inhalt der Klammern mit einem Zeilenumbruch
+    println("Hallo Welt!")
+    println("Hello World!")
+    println("Hola mundo") // hier immer zeilenumbruch hinter
+
+    // print: druckt Inhalt der Klammern ohne Zeilen
+    print("x") // hier kein Zeilenumbruch und kein Leerzeichen vor dem nächsten print
+    print("Ich ")
+    print("bin ")
+    print("müde...")
+    println()
+
+
+    // Compilerfehler / Kompilierfehler
+    // pirntln("Ups tippfehler...")
+
 
 }
