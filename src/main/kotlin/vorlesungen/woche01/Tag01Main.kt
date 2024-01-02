0fd98fa194daa32854924a89971088d92a4bd1d6
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-01-02 07:58:46 +0000

    initial commit

---
+++ src/main/kotlin/vorlesungen/woche01/Tag01Main.kt
@@ -0,0 +1,11 @@
+package vorlesungen.woche01
+
+/*
+    main Funktion: der Ort, an dem unser Code ausgeführt wird
+    jeglicher Code muss hier rein (zumindest, bis wir weitere Funktionen kennenlernen)
+ */
+fun main() {
+
+    println("hi")
+
+}
