58a4eda305e79293e8075faff0c6b20f8328d900
luk <lukas.richter@syntax-institut.de>
2024-01-04 15:37:14 +0100

    Wiederholung

--- src/main/kotlin/wiederholungen/woche01/Tag03-Operationen.kt
+++ src/main/kotlin/wiederholungen/woche01/Tag03-Operationen.kt
@@ -1,17 +1,17 @@
 
 fun main() {
 
-
     println("Gib einen Wer ein: ")
-    var a = readln()
-    val b = 8.89
-    println("Das Ergebnis der beiden Zahlen ist ${a + b}")
+    var a = readln().toDouble() // Typkonvertierung
+    val b: Double = 8.89
+    println("Das Ergebnis der beiden Zahlen ist ${a + b}") // $ bedeutet Stringinterpolation
 
+    /* Block Kommentar */
 
     // Normales Kommentar
 
     var x = readln()
-    var y = 10
+    var y = 100
     var result = x + y
     println("die lösung ist $result")
 }
