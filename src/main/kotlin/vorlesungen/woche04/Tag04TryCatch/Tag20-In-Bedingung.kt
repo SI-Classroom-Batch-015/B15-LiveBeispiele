8930b080f84a075fe8da81c0454244277891f716
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-01-25 11:51:41 +0000

    Woche 4 Tag 4: Nachtrag (nicht in der VL geschafft)

--- src/main/kotlin/vorlesungen/woche04/Tag04TryCatch/Tag20-In-Bedingung.kt
+++ src/main/kotlin/vorlesungen/woche04/Tag04TryCatch/Tag20-In-Bedingung.kt
@@ -1,25 +1,38 @@
 package VORLESUNG.Woche04.Tag20Wiederholung
 
+
 fun main(){
 
     // Beispiel 1: einfachstes in-Beispiel
-
+    val inString = "In diesem String ist in drin"
+    var x = "in" in inString
+    if (x){
+        println("Ja, es ist in in diesem String")
+    } else {
+        println("Nein, kein in in diesem String.")
+    }
 
     // Beispiel 2: einfachstes in-Beispiel
-
+    val numbers = listOf(1,2,3,4,5,6,7,8,9)
+    if (3 in numbers) println("Die 3 ist vorhanden")
 
     // Beispiel 3: In-Beispiel mit Listenindizen
     val namensListe: List<String> = listOf("Stephan", "Merlin", "Hannes", "Emily", "Nico", "Benni", "Brian")
     val notenListe: List<Int> = listOf(1, 2, 3, 4, 1, 2, 3)
     // Wie muss ich vorgehen, um an die Note einer Person ranzukommen?
 
-
-
-
-
-
-
-
+    println("Bitte Namen, der Person, deren Note du erfahren willst, eingeben...")
+    // 1. Schritt: Namen in Variable speichern
+    val gesuchterName = readln()
+    if (gesuchterName in namensListe){
+        // 2. Schritt: Index des Namens in der namensListe in Variable speichern
+        val index: Int = namensListe.indexOf(gesuchterName) // Im Falle Merlin: Index = 1
+        // 3. Schritt: über den Index an die entsprechende Note in der Notenliste rankommen:
+        val note = notenListe[index]
+        println("$gesuchterName hat die Note $note")
+    } else {
+        println("$gesuchterName existiert nicht im Klassenbuch.")
+    }
 
     // Beispiel 4: In-Beispiel mit Map
     val sortiment = mutableMapOf(
@@ -29,9 +42,30 @@ fun main(){
         "Brezel" to 0.75
     )
     // Neues Produkt zum Sortiment hinzufügen
-
+    println("Bitte Produkt eingeben...")
+    val newProduct = readln()
+    println("Bitte Preis eingeben...")
+    val newPrice = readln().toDouble()
 
     // falls das produkt bereits im sortiment ist: den preis einfach aktualisieren
+    if (newProduct in sortiment) { // sortiment.contains(newProduct)
+        println("Das Produkt existiert bereits")
+        if (sortiment[newProduct] != newPrice) {
+            sortiment[newProduct] = newPrice
+            println("Der Preis wurde aktualisiert!")
+        }
+    } else { // abfangen, dass ein neues Produkt hinzugefügt wird
+        sortiment[newProduct] = newPrice
+        println("Das Produkt $newProduct wurde neu hinzugefügt.")
+
+    }
+
+    if (sortiment["Schokolade"] != null) {
+        println("Schokolade ist vorhanden")
+    } else {
+        println("Schokolade nicht vorhanden")
+    }
+
 
 
 }
--- src/main/kotlin/vorlesungen/woche04/Tag04TryCatch/try-Catch.kt
+++ src/main/kotlin/vorlesungen/woche04/Tag04TryCatch/try-Catch.kt
@@ -2,21 +2,21 @@ package vorlesungen.woche04.Tag04TryCatch
 
 fun main(){
 
-//    val list = listOf(1,2,3,4,5,6,7,8) // der Index geht von 0 bis 7
-//
-//    try {
-//        triggerIndexOutOfBoundsEx(list)
-//    } catch (e: Exception) {
-//   //     println("Fehler in der main abgefangen, der aus der Funktion triggerIOOBE() rausgeschmissen wurde...")
-//    }
-//
-//
-//    println("...")
-//    println("...")
-//    println("...")
-//    println("Fehler erfolgreich abgefangen, der Code ist weiter gelaufen...")
-//
-//
+    val list = listOf(1,2,3,4,5,6,7,8) // der Index geht von 0 bis 7
+
+    try {
+        triggerIndexOutOfBoundsEx(list)
+    } catch (e: Exception) {
+   //     println("Fehler in der main abgefangen, der aus der Funktion triggerIOOBE() rausgeschmissen wurde...")
+    }
+
+
+    println("...")
+    println("...")
+    println("...")
+    println("Fehler erfolgreich abgefangen, der Code ist weiter gelaufen...")
+
+
     println(triggerNullPointerException("Hallo es ist Donnerstag"))
     println(triggerNullPointerException(null))
 
@@ -30,6 +30,7 @@ fun triggerIndexOutOfBoundsEx(list: List<Int>): Int {
         println("Index-Fehler innerhalb der Funktion aufgetreten! Exception: $e")
         // throw = werfen, schmeißen: Fehler wird doch geworfen, Programm stürzt ab
         throw e
+        var x = 9 // unerrreichbarer Code: throw ist wie return, danach wird aus dem Block rausgesprungen
     } catch (e: NullPointerException) {
         println("Null-Fehler innerhalb der Funktion aufgetreten: $e")
     } catch (e: Exception) {
@@ -48,3 +49,38 @@ fun triggerNullPointerException(string: String?): Int {
     }
 
 }
+
+fun triggerNumberFormatEx(zahl: String ) : Int {
+    try {
+        return zahl.toInt()
+    }
+    catch (e: Exception){
+        println("Fehler in triggerNumberFormatEx: $e")
+        println("Bitte gib eine andere Zahl ein:")
+        var input = readln()
+        triggerNumberFormatEx(input)
+        // alternative: return 0 // statt dem gewollten Int einfach 0 returnen, damit das Programm weiterlaufen kann
+    }
+    return 0
+}
+
+fun triggerArithmeticEx() : Int {
+    var result = 0
+    try {
+        result = 10 / 0
+        println("Ergebnis der Rechnung: $result")
+        return result
+    } catch (e: Exception) {
+        println("Oh nein, Fehler in triggerArithmeticEx! $e")
+        // Standardmäßig -1 in result schreiben, falls was schief läuft
+        result = -1
+
+    }
+
+    println("Hier kommen wir nur an, wenn das Programm nicht abstürzt")
+    println("Result ist jetzt $result")
+    return result
+}
+
+
+
