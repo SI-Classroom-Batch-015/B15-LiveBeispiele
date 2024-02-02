265366051181232285a5b1e3aa3e9c1445b556ea
luk <lukas.richter@syntax-institut.de>
2024-02-02 16:02:42 +0100

    wdh

--- src/main/kotlin/vorlesungen/woche05/Tag03/ComputationalThinking/Tag24-DurchschnittUndFilter.kt
+++ src/main/kotlin/vorlesungen/woche05/Tag03/ComputationalThinking/Tag24-DurchschnittUndFilter.kt
@@ -7,7 +7,29 @@ fun main() {
         Pair("Charlie", 40),
         Pair("David", 22)
     )
-    // Durchschnittsalter aller Leute über zB 30 rausfinden
+    // Durchschnittsalter
 
+    val averageAge = namesAndAges.map { it.second }.average()
+    // map -> transformiert die liste und gibt eine neue aus
+
+    println("Durchschnittsalter ist $averageAge")
+}
+/*
+fun calculateAverageAge(namesAndAges: List<Pair<String, Int>>): Double {
+    val totalAge = namesAndAges.sumBy { it.second }
+    return totalAge.toDouble() / namesAndAges.size
 }
 
+fun main() {
+    val namesAndAges = listOf(
+        Pair("Alice", 30),
+        Pair("Bob", 25),
+        Pair("Charlie", 40),
+        Pair("David", 22)
+    )
+
+    val averageAge = calculateAverageAge(namesAndAges)
+
+    println("Der Durchschnitt des Alters beträgt: $averageAge")
+}
+*/
